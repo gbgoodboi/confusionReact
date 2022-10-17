d7f9643f5bb4f7f917426b64588ddb9c9dde7dd0
Gi <baotdgde160069@fpt.edu.vn>
2022-10-17 21:33:09 +0700

    Single Page Application Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
         if (dish != null) {
             return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle> {dish.name}</CardTitle>
-                        <CardText> {dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <div className='col-12 col-md-5 m-1'>
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle> {dish.name}</CardTitle>
+                            <CardText> {dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+                
             );
         }
         else {
@@ -35,7 +38,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         })
         
         return (
-            <div>
+            <div className='col-12 col-md-5 m-1'>
                 <h4>Comments</h4>
                 <ul className='list-unstyled'>
                     {cmt}
@@ -45,7 +48,6 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
     }
 
     const DishDetail = (props) => {
-        console.log('DishDetail Component render invoked')
         if (props.dish == null) {
             return <div></div>;
         }
@@ -53,13 +55,19 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         return (
             <div className='container'>
                 <div className='row'>
-                <div className='col-12 col-md-5 m-1'>
-                    <RenderDish dish={props.dish}></RenderDish>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                       <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className='col-12'>
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
                 </div>
-                <div className='col-12 col-md-5 m-1'>
-                    <RenderComments comment = {props.dish.comments}></RenderComments>
+                <div className='row'>
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comment={props.comments} />
                 </div>
-            </div>
             </div>         
         );
     }
