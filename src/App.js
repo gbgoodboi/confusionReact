b0bca50fdb67d3df3bda89a92ccaafc63b021024
Gi <baotdgde160069@fpt.edu.vn>
2022-10-17 11:22:03 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Main from './components/MainComponent';
-
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
 
   render() {
     return (
-      <div>
+      <BrowserRouter>
+        <div>
         <Main />
-      </div>
+        </div>
+      </BrowserRouter>
     );
+
   }
 }
 
